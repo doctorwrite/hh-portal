6c5d3d95ffeaaf5a7d75ec95e8e366dbe4c584cc
doctorwrite <hiphoprecords@yandex.ru>
2026-08-26 16:41:39 +0700

    Add files via upload

---
+++ components/interactive/index.ts
@@ -0,0 +1,6 @@
+// components/interactive/index.ts
+// Экспорт всех виджетов (будем добавлять по мере создания)
+
+// export { EQWidget } from './EQWidget'
+// export { CompressorWidget } from './CompressorWidget'
+// ... и так далее
